c6d6435b14c22f2985c58dc074af161d2ecbe7b3
IamShaDoW666 <www.gaming2018@gmail.com>
2024-10-02 12:31:04 +0530

    init

---
+++ src/app/_components/posts.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { api } from "@/trpc/react";
+
+
+const Posts = () => {
+    const [posts] = api.post.getAll.useSuspenseQuery();
+  return (
+    <div className="grid grid-cols-3 gap-4 rounded bg-zinc-800 p-12">
+        {posts.map((post) => <div className="text-xl text-white font-bold">
+            {post.name}
+        </div>)}
+    </div>
+  );
+};
+
+export default Posts

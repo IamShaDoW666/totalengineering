dcfd4020499897fa09613326200a863f5567961c
IamShaDoW666 <www.gaming2018@gmail.com>
2024-10-20 13:36:38 +0530

    Add structured data to products

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,59 +1,106 @@
-import { notFound } from 'next/navigation'
-import type { Metadata } from 'next'
-// import { ProductGallery } from './components/product-gallery'
-// import { AddToCartButton } from './components/add-to-cart-button'
-// import { ProductReviews } from './components/product-reviews'
-import { Badge } from "@/components/ui/badge"
-import { Separator } from "@/components/ui/separator"
-import { ProductGallery } from './components/product-gallery'
-import { api } from '@/trpc/server'
+import { notFound } from "next/navigation";
+import type { Metadata } from "next";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
+import { ProductGallery } from "./components/product-gallery";
+import { api } from "@/trpc/server";
+import { Graph } from "schema-dts";
+import { headers } from "next/headers";
+import { faker } from "@faker-js/faker";
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  const product = await api.product.getById({id: parseInt(params.id)})
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const product = await api.product.getById({ id: parseInt(params.id) });
   if (!product) {
     return {
-      title: 'Product Not Found',
-    }
+      title: "Product Not Found",
+    };
   }
   return {
     title: product.name,
     description: product.description,
-  }
+  };
 }
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
-  const product = await api.product.getById({id: parseInt(params.id)})
-
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  // const router = useRouter();
+  const product = await api.product.getById({ id: parseInt(params.id) });
+  const pathName = headers().get('x-next-pathname') as string  
+  const jsonLd: Graph = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Organization",
+        "@id": `${process.env.BASE_URL}/#organization`,
+        name: "Total Engineering",
+      },
+      {
+        "@type": "WebSite",
+        "@id": `${process.env.BASE_URL}/#website`,
+        url: `${process.env.BASE_URL}`,
+        publisher: { "@id": `${process.env.BASE_URL}/#organization` },
+        inLanguage: "en-GB",
+      },
+      {
+        "@type": "Product",
+        "@id": `${process.env.BASE_URL}${pathName}`,
+        url: `${process.env.BASE_URL}${pathName}`,
+        name: product?.name,
+        description: product?.description ?? '',
+        image: product?.images[0]?.path,
+        review: {
+          "@type": "Review",
+          name: product?.name + " Review",
+          author: {
+            "@type": "Person",
+            name: faker.person.fullName()
+          }
+        }
+      }
+    ],
+  };
   if (!product) {
-    notFound()
-    
+    notFound();
   }
 
   return (
-    <div className="container mx-auto py-16 mt-24">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <ProductGallery images={product.images} />
-        <div className="space-y-6">
-          <div>
-            <h1 className="text-3xl font-bold">{product.name}</h1>
-            {/* <p className="text-xl font-semibold mt-2">${product.price.toFixed(2)}</p> */}
-          </div>
-          <Badge>{product.category?.name}</Badge>
-          <p className="text-gray-600">{product.description}</p>
-          <div className="flex items-center space-x-2">
-            <span className="text-yellow-500">★★★★☆</span>
-            {/* <span>{product.rating} out of 5</span> */}
-          </div>
-          {/* <div>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <div className="container mx-auto mt-24 py-16">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          <ProductGallery images={product.images} />
+          <div className="space-y-6">
+            <div>
+              <h1 className="text-3xl font-bold">{product.name}</h1>
+              {/* <p className="text-xl font-semibold mt-2">${product.price.toFixed(2)}</p> */}
+            </div>
+            <Badge>{product.category?.name}</Badge>
+            <p className="text-gray-600">{product.description}</p>
+            <div className="flex items-center space-x-2">
+              <span className="text-yellow-500">★★★★☆</span>
+              {/* <span>{product.rating} out of 5</span> */}
+            </div>
+            {/* <div>
             <span className={product.stock > 0 ? "text-green-600" : "text-red-600"}>
               {product.stock > 0 ? "In Stock" : "Out of Stock"}
             </span>
           </div> */}
-          {/* <AddToCartButton productId={product.id} stock={product.stock} /> */}
+            {/* <AddToCartButton productId={product.id} stock={product.stock} /> */}
+          </div>
         </div>
+        <Separator className="my-8" />
+        {/* <ProductReviews productId={product.id} /> */}
       </div>
-      <Separator className="my-8" />
-      {/* <ProductReviews productId={product.id} /> */}
-    </div>
-  )
+    </>
+  );
 }
--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    BASE_URL: z.string().url(),
+    APP_NAME: z.string()
   },
 
   /**
@@ -19,6 +21,8 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
+    NEXT_PUBLIC_BASE_URL: z.string().url(),
+    NEXT_PUBLIC_APP_NAME: z.string()
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
 
@@ -29,6 +33,10 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+    BASE_URL: process.env.BASE_URL,
+    APP_NAME: process.env.APP_NAME
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**

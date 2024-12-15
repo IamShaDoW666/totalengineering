aef216e458860a7953bfaf477f027755d241b7dc
nibhapr <nibhapr@gmail.com>
2024-12-15 19:43:10 +0530

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+
+export const siteUrl = 'https://totaleng.in';
+export const generateRobotsTxt = true;
+export const sitemapSize = 7000;
+export const changefreq = 'daily';
+export const priority = 0.7;
+  
